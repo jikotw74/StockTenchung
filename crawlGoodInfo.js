d29153cb256db57bd94c96804a87b2e7ac8a6022
Lu Yi-Te <jikotw74@gmail.com>
2017-12-02 20:56:06 +0800

    update

--- crawlGoodInfo.js
+++ crawlGoodInfo.js
@@ -147,6 +147,56 @@ async function crawlGoodInfo(stock_ids){
     return result;
 }
 
-Promise.all([crawlGoodInfo(['2327', '2890', '6180'])])
+async function crawlMain(stock_ids){
+    // console.log('[getStocks ids] ', stock_ids);
+    let result = false;
+
+    stock_ids = [].concat(stock_ids);
+    if(stock_ids.length > 0){
+        let queryKeys = [];
+
+        // start brwoser
+        browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            // headless: false,
+            // slowMo: 250 // slow down by 250ms
+        });
+
+        // get cookies
+        const page = await browser.newPage();
+        await page.goto(`http://www.wantgoo.com/stock/astock/agentstat2?stockno=${stock_ids[0]}`);
+        // const cookies = await page.cookies();
+        // console.log(cookies);
+
+        const bodyHandle = await page.$('body');
+        const html = await page.evaluate(body => body.innerHTML, bodyHandle);
+        
+        await page.close();
+
+        await htmlToJson.parse(html, {
+            'messages': function ($doc) {
+                // console.log(this);
+                let trArray = this.map('#listMain tbody tr', function($row, index){
+                    return $row.children().toArray().innerHTML;
+                });
+
+                return trArray.filter((item, index) => index > 2 && index < 6)
+            }
+        })
+        .then(function(data){
+            console.log(data);
+            result = data;
+        });
+
+        // console.log('[getStocks queryKeys] ', queryKeys);
+
+        await browser.close();
+    }
+    
+    // console.log('[getStocks return] ', result);
+    return result;
+}
+
+Promise.all([crawlMain(['2327', '2890', '6180'])])
 .then(message => console.log(message))
 .catch(error => console.log(error))
